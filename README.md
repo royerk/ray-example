3c994d26d0be5151a0a90fd5f4f5ec2c60f2c4ba
Lysk <kroyer.github@gmail.com>
2020-06-07 15:35:31 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Use Ray for multithread with python
+
+## Install & run
+
+Make a virtualenv with python 3, run `pip install -r requirements.txt`.
+
+Run with `python remote_class_1.py`, parameters:
+* `--followers 2`
+* `--start 30` first value computed will be `fibonacci(30)`
+* `--end 38` last value computed will be `fibonacci(38)`
+* or run `python remote_class_1.py --help` for list of parameters
+
+## Leader
+
+The leader has a list of its followers. Each follower has the `ray.remote` annotation.
+The leader ask followers to run some computation then wait for results.
+As soon as result is processed, it's printed, the follower is asked to compute on a new value.
+
+## Follower
+
+The follower has no knowledge of the leader.
+
+## Notes
+
+* a follower computes a value only when asked to do so, no internal thread that keeps computing stuff
+* use `htop` to see cpu usage, quite fun
+
+## Resources
+
+* Ray example with A3C, kinda a bit complex to get started [official doc, a3c example](https://docs.ray.io/en/latest/auto_examples/plot_example-a3c.html)
+
+## Why going through the trouble?
+
+I adapted this code ([A3C with tensorflow](https://blog.tensorflow.org/2018/07/deep-reinforcement-learning-keras-eager-execution.html)) to tensorflow 2. It's great for understanding and it's limited to python multi-processing.
+To get better speed, I thought the first step would be to use multi-threading. It's a smaller step than going straight to distributed.
+Ray framework may deliver that.
---
+++ remote_class_1.py
@@ -0,0 +1,113 @@
+import ray
+from datetime import datetime
+import argparse
+
+
+@ray.remote
+class Follower:
+    def __init__(self, _id):
+        self.id = _id
+
+    def fibonacci(self, n):
+        """
+        This function should be slow for proper demonstration.
+        """
+        if n == 1 or n == 2:
+            return 1
+        return self.fibonacci(n - 1) + self.fibonacci(n - 2)
+
+    def follower_fibonacci(self, n):
+        """
+        Wrapper around call to fibonacci.
+        @param n: parameter to fibonacci
+        @return n requested, fibonacci(n), follower id
+        """
+        return n, self.fibonacci(n), self.id
+
+
+class Leader:
+    def __init__(self, n_followers):
+        """
+        For simplicity sake, n cpu == n followers.
+        @param n_followers: number of followers
+        """
+        ray.init(num_cpus=n_followers)
+        self.followers = [Follower.remote(i) for i in range(n_followers)]
+
+    def get_fibonacci_values(self, start_n=20, end_n=30):
+        """
+        Ask followers to compute fibonacci values. Here followers are
+        idle after submitting their answer.
+        @param start_n: first value to compute
+        @param end_n: last value to compute
+        """
+        received_answers = 0
+        expected_answers = end_n - start_n
+        print("{} answers expected".format(expected_answers))
+
+        fibonacci_list = []
+        # initialize by asking followers to compute first values
+        for follower in self.followers:
+            fibonacci_list.append(follower.follower_fibonacci.remote(start_n))
+            start_n += 1
+
+        # here the stop condition is reached when we get the number of expected answer
+        # another option is to have while True and an if condition: run more else: break
+        while received_answers < expected_answers:
+            # collect first result as soon as it's possible
+            # keep track in the list of the other results
+            done_id, fibonacci_list = ray.wait(fibonacci_list)
+
+            # parse answer from follower and print
+            n_fib, answer_fibonacci, follower_id = ray.get(done_id)[0]
+            received_answers += 1
+            start_n += 1
+            print(
+                "Follower {}: fib({})={}, received answers: {}".format(
+                    follower_id, n_fib, answer_fibonacci, received_answers
+                )
+            )
+
+            # ask the follower for another value
+            if start_n <= end_n:
+                fibonacci_list.extend(
+                    [self.followers[follower_id].follower_fibonacci.remote(start_n)]
+                )
+
+
+def main(n_followers=2, start_n=30, end_n=38):
+    start_date = datetime.now()
+    leader = Leader(n_followers)
+
+    leader.get_fibonacci_values(start_n, end_n)
+
+    end_date = datetime.now()
+
+    print(
+        "{} followers, duration: :{}".format(
+            len(leader.followers), end_date - start_date
+        )
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run a leader and followers to get fibonacci values."
+    )
+    parser.add_argument(
+        "--followers",
+        default=2,
+        type=int,
+        help="Choose number of followers (== number of cpu).",
+    )
+    parser.add_argument(
+        "--start", default=20, type=int, help="Start getting fibonacci at this value",
+    )
+    parser.add_argument(
+        "--end", default=30, type=int, help="Last value to compute",
+    )
+
+    args = parser.parse_args()
+    print(args)
+
+    main(n_followers=args.followers, start_n=args.start, end_n=args.end)
